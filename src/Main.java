730235712e81dd5e43c3e55d2e924c0048b58c02
alexDy1 <kratos19982571@gmail.com>
2021-10-13 11:33:32 -0400

    Ejercicio recurcividad

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] a = {2, 9, 4};
+        int[] b = {2, 9, 4};
+        boolean diferencia = Ejercicio.compararVectoresRecurcividad(a,b);
+
+        System.out.println(diferencia);
+
+    }
+
+
+
+}
